c2686029ee45040cbeefb46e1be432e78269b711
M. Bilal Benli <180403075@ogr.gelisim.edu.tr>
2021-12-20 02:24:50 +0300

    core interface created.

--- ProgrammingHomiesRestApi/Dtos/UserDtos/CreateUserDto.cs
+++ ProgrammingHomiesRestApi/Dtos/UserDtos/CreateUserDto.cs
@@ -1,13 +1,20 @@
-﻿namespace ProgrammingHomiesRestApi.Dtos.UserDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgrammingHomiesRestApi.Dtos.UserDtos
 {
-    public record UserDto
+    public class CreateUserDto
     {
         public Guid Id { get; set; }
+        [Required]
         public string Username { get; init; }
+        [Required]
         public string Password { get; init; }
         public Uri ProfilePhotoUrl { get; init; }
         public DateTime BirthDate { get; init; }
+        [Required]
         public string Mail { get; init; }
         public string Biography { get; init; }
+
+
     }
 }
