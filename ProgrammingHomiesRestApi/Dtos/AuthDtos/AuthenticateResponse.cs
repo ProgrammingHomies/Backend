0f8c1d2d7ca2c9ede0bd79648f8b7133efdd94d1
DogaKaymaz <71275551+DogaKaymaz@users.noreply.github.com>
2022-01-03 04:40:47 +0300

    Authentication

---
+++ ProgrammingHomiesRestApi/Dtos/AuthDtos/AuthenticateResponse.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammingHomiesRestApi.Entities;
+
+namespace phra.Dtos.AuthDtos
+{
+    public class AuthenticateResponse
+    {
+        public string Token { get; set; }
+
+        public AuthenticateResponse(User user, string token)
+        {
+            Token = token;
+        }
+    }
+}
